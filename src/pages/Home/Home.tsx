54a909e497fe20ca91685e20e67b06521ce43283
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2025-12-13 19:06:51 +0200

    chore: delete unused import

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import SearchInput from "../../components/SearchInput/SearchInput";
 import type { MovieShort, SearchResponse } from "../../types";
 import { searchMovie } from "../../api/movies";
