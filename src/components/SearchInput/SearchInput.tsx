d0f3a057d693ec981a92e7ccd89b43fa3ca409e2
Bogdan ProBook <bogdankabachenko2015@gmail.com>
2025-12-15 16:24:36 +0200

    feat: add search functionality

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,10 +1,43 @@
-const SearchInput = () => {
+import { useState, type KeyboardEvent } from "react";
+import s from "./SearchInput.module.scss";
 
-  return (
-    <>
-      
-    </>
-  )
+interface SearchInputProps {
+  onSearch: (searchText: string) => void;
+  initialValue?: string;
 }
 
-export default SearchInput
+const SearchInput = ({ onSearch, initialValue = ""}: SearchInputProps) => {
+  const [inputText, setInputText] = useState(initialValue);
+
+  const handleSearch = () => {
+    const trimmed = inputText.trim();
+    onSearch(trimmed);
+
+    if (trimmed.length > 0) {
+      setInputText(trimmed);
+    }
+  };
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  return (
+      <div className={s.inputBlock}>
+        <input
+          className={s.input}
+          type="text"
+          onChange={(e) => setInputText(e.target.value)}
+          onKeyDown={handleKeyDown}
+          value={inputText}
+          placeholder="Type title of movie"
+        />
+        <button className={s.button} type="button" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
+  );
+};
+
+export default SearchInput;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,11 +3,15 @@ import SearchInput from "../../components/SearchInput/SearchInput";
 import type { MovieShort, SearchResponse } from "../../types";
 import { searchMovie } from "../../api/movies";
 import MovieList from "../../components/MovieList/MovieList";
+import { useSearchParams } from "react-router";
 
 const Home = () => {
   const [movies, setMovies] = useState<MovieShort[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const searchText = searchParams.get("search") || "";
 
   useEffect(() => {
     const fetchMovies = async (title: string) => {
@@ -16,7 +20,10 @@ const Home = () => {
         setError(null);
 
         const data: SearchResponse = await searchMovie(title);
-        setMovies(data.Search);
+        const filteredData = data.Search.filter(
+          (movie, index, self) => index === self.findIndex((m) => m.imdbID === movie.imdbID)
+        );
+        setMovies(filteredData);
       } catch (error) {
         if (error instanceof Error) {
           setError(error.message);
@@ -27,17 +34,30 @@ const Home = () => {
         setIsLoading(false);
       }
     };
-    fetchMovies("Matrix");
-  }, []);
 
-  if (!isLoading && !error) {
-    console.log(movies);
-  }
+    if (searchText) {
+      fetchMovies(searchText);
+    } else {
+      setMovies([]);
+      setError(null);
+    }
+  }, [searchText]);
+
+  const onSearch = (searchText: string) => {
+    if (searchText.trim().length > 0) {
+      setSearchParams({ search: searchText });
+    } else {
+      setSearchParams({});
+    }
+  };
 
   return (
     <>
-      <SearchInput />
-      <MovieList list={movies}/>
+      <SearchInput onSearch={onSearch} initialValue={searchText} />
+      {isLoading && <h3>Please wait... Your list are loading.</h3>}
+      {searchText === null && <h3>Please type your request title.</h3>}
+      {error && searchText && <h3>Error. {error}</h3>}
+      {!isLoading && !error && <MovieList list={movies} />}
     </>
   );
 };
