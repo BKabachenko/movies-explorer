5f89cc5146f0e1c5ac0fae05fd617bdb92f59bb0
Bogdan ProBook <bogdankabachenko2015@gmail.com>
2026-01-14 19:11:34 +0200

    fix: remove reset.css style: sort imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import "./reset.css";
-import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
 
-createRoot(document.getElementById("root")!).render(
+import App from './App.tsx';
+
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
