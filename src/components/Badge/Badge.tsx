0184ed6d165519738f8578c8eb83ec1bc9eafb64
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-01-21 15:13:15 +0200

    refactor(Badge): replace icon prop with children

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,10 +1,9 @@
 interface Badge {
-  icon?: string;
-  children: string;
+  children: React.ReactNode;
   variant: 'genre' | 'rate' | 'secondary' | 'author';
 }
 
-const Badge = ({ icon, children, variant }: Badge) => {
+const Badge = ({ children, variant }: Badge) => {
   const variants = {
     genre: 'rounded-2xl border border-indigo-200 bg-indigo-100 text-indigo-700 uppercase',
     rate: 'rounded-2xl border border-yellow-200 bg-yellow-100 text-yellow-800 uppercase',
@@ -12,11 +11,10 @@ const Badge = ({ icon, children, variant }: Badge) => {
     author: 'rounded-2xl border border-gray-200 bg-transparent text-black shadow-sm',
   };
   const defaultStyle =
-    'flex text-center flex-wrap content-center justify-center p-1 px-4 text-sm font-semibold';
+    'flex text-center flex-wrap content-center justify-center p-1 px-4 text-sm font-semibold gap-2';
   const badgeStyle = variants[variant] + ' ' + defaultStyle;
   return (
     <div className={badgeStyle}>
-      {icon && <img className='mb-1 h-4 w-auto translate-y-0.5 sm:mr-2 sm:mb-0' src={icon} />}
       {children}
     </div>
   );
