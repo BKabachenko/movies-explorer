1c294522644893454c16dd9fdad2085b0cfe19ff
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-01-21 15:12:56 +0200

    feat(ui): create reusable Icon wrapper component

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface IconProps {
+  src: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  size?: 'sm' | 'md' | 'lg';
+  className?: string;
+}
+
+const Icon = ({ src: SvgIcon, size = 'sm', className }: IconProps) => {
+  const sizeClasses = {
+    sm: 'w-4 h-4',
+    md: 'w-6 h-6',
+    lg: 'w-10 h-10',
+  };
+
+  return (
+    <>
+      <SvgIcon className={`${sizeClasses[size]} ${className} shrink-0 transition-colors translate-y-0.5`} />
+    </>
+  );
+};
+
+export default Icon;
