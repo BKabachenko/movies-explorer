d7ff5e5593a6b55d5568ae3ce59896f4bf47ec15
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-01-22 17:37:37 +0200

    style(SearchInput): style input and btn and add icon to btn

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,12 +1,14 @@
-import { useState, type KeyboardEvent } from "react";
-import s from "./SearchInput.module.scss";
+import { type KeyboardEvent, useState } from 'react';
+
+import searchIcon from '../../assets/icons/search.svg?react';
+import Icon from '../Icon/Icon';
 
 interface SearchInputProps {
   onSearch: (searchText: string) => void;
   initialValue?: string;
 }
 
-const SearchInput = ({ onSearch, initialValue = ""}: SearchInputProps) => {
+const SearchInput = ({ onSearch, initialValue = '' }: SearchInputProps) => {
   const [inputText, setInputText] = useState(initialValue);
 
   const handleSearch = () => {
@@ -18,25 +20,30 @@ const SearchInput = ({ onSearch, initialValue = ""}: SearchInputProps) => {
     }
   };
   const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
+    if (e.key === 'Enter') {
       handleSearch();
     }
   };
 
   return (
-      <div className={s.inputBlock}>
-        <input
-          className={s.input}
-          type="text"
-          onChange={(e) => setInputText(e.target.value)}
-          onKeyDown={handleKeyDown}
-          value={inputText}
-          placeholder="Type title of movie"
-        />
-        <button className={s.button} type="button" onClick={handleSearch}>
-          Search
-        </button>
-      </div>
+    <div className='flex w-full flex-row content-center items-center justify-center gap-4'>
+      <input
+        className='peer focus-visible:shadow-inset w-full rounded-lg border-2 border-indigo-300 bg-gray-100 p-1 px-3 transition-all focus-visible:border-indigo-600 focus-visible:bg-white focus-visible:inset-shadow-sm focus-visible:inset-shadow-indigo-200 focus-visible:outline-none'
+        type='text'
+        onChange={(e) => setInputText(e.target.value)}
+        onKeyDown={handleKeyDown}
+        value={inputText}
+        placeholder='Type title of movie'
+      />
+      <button
+        className='flex cursor-pointer flex-row items-center gap-1 rounded-lg border border-indigo-200 p-1 px-3 peer-focus-visible:bg-indigo-400 transition peer-focus-visible:text-white'
+        type='button'
+        onClick={handleSearch}
+      >
+        <Icon src={searchIcon} className=''/>
+        Search
+      </button>
+    </div>
   );
 };
 
