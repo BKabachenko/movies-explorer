9e953bbf97df0276e6427d0084f62e2593567f44
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-01-22 10:42:47 +0200

    feat(Header): implement logo and responsive design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import logo from '../../assets/icons/main-logo.svg?react';
+import Icon from '../Icon/Icon';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className='flex w-full flex-col content-center justify-center gap-2 p-2 px-10 md:flex-row'>
+      <div className='flex flex-1 content-center justify-center'>
+        <div className='flex h-8 w-8 content-center justify-center rounded-lg bg-indigo-800'>
+          <Icon src={logo} className='h-full translate-0 !translate-y-0 text-white' />
+        </div>
+        <p className='p-1 font-bold'>
+          Kino<span className='text-indigo-800'>Base</span>
+        </p>
+      </div>
+      <div className='flex flex-5 content-center justify-center border border-red-500'>search</div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
