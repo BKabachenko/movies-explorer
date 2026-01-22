ef129518e25fec7d7a98c5db1939e9ce7373a292
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-01-22 10:44:57 +0200

    style(BackBtn): fix my to mb

--- src/components/BackBtn/BackBtn.tsx
+++ src/components/BackBtn/BackBtn.tsx
@@ -7,7 +7,7 @@ const BackBtn = () => {
   const navigate = useNavigate();
   return (
     <button
-      className='group my-5 flex h-auto w-20 cursor-pointer flex-row justify-center gap-1 rounded-md border border-gray-200 bg-white p-2 font-semibold text-gray-500 shadow-sm transition-colors hover:bg-indigo-50 hover:text-indigo-600 active:bg-indigo-200 active:text-indigo-600'
+      className='group mb-5 flex h-auto w-20 cursor-pointer flex-row justify-center gap-1 rounded-md border border-gray-200 bg-white p-2 font-semibold text-gray-500 shadow-sm transition-colors hover:bg-indigo-50 hover:text-indigo-600 active:bg-indigo-200 active:text-indigo-600'
       type='button'
       onClick={() => navigate(-1)}
     >
