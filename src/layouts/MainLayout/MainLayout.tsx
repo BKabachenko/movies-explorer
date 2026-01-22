c3b18f789c4491aebdabd8a93a6241597e65d270
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-01-22 18:34:47 +0200

    style(MainLayout): add bg color

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,15 +1,16 @@
-import { Outlet } from "react-router"
-import Header from '../../components/Header/Header'
+import { Outlet } from 'react-router';
+
+import Header from '../../components/Header/Header';
 
 const MainLayout = () => {
   return (
-    <>
-      <Header/>
+    <div className='bg-gray-200'>
+      <Header />
       <main>
         <Outlet />
       </main>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default MainLayout
+export default MainLayout;
