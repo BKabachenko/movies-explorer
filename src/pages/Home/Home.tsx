8e68402b174af317786731523076c4f5e048dbf3
Bogdan ProBook <bogdankabachenko2015@gmail.com>
2026-01-24 20:22:29 +0200

    feat(Home): create some shit func, just save

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,46 @@
+import { useEffect, useState } from 'react';
+import { getMovieById } from '../../api/movies';
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const [movies, setMovies] = useState<string>();
 
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
 
-  
+        const response = await fetch('/data/top1000movies.json');
+        if (!response.ok) {
+          throw new Error('File does not exist.');
+        }
+        const data = await response.json();
+        setMovies(data);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        } else {
+          setError('Unknown error');
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
 
   return (
     <>
-      <div className=''>Home Page</div>
+      {!isLoading && 
+      <div className=''>
+        <p>TOP 10</p>
+
+      </div>
+      }
     </>
   );
 };
