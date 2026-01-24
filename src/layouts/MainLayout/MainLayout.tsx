b9d1406ab48d56891d3112c7bc05a866452a0da9
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-01-24 13:41:14 +0200

    style(MainLayout): stretch main width and push footer to bottom

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -5,9 +5,9 @@ import Header from '../../components/Header/Header';
 
 const MainLayout = () => {
   return (
-    <div className='bg-gray-200 min-h-dvh'>
+    <div className='grid min-h-dvh grid-rows-[auto_1fr_auto] bg-gray-200'>
       <Header />
-      <main>
+      <main className='mx-auto mb-10 w-full max-w-5xl px-5 md:mb-20 lg:mb-30'>
         <Outlet />
       </main>
       <Footer />
