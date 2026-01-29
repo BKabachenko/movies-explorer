f59c9d87d5a0897ebd6e1a596880046feebdbcd8
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-01-29 13:27:41 +0200

    refactor(MovieCard): destructure movie properties for cleaner code

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -9,33 +9,35 @@ type MovieCardProps = {
 const dummyPosterLink = 'https://dummyimage.com/300x450/787878/ffffff&text=No+Preview';
 
 const MovieCard = ({ movie }: MovieCardProps) => {
-  const posterLink =
-    movie.Poster === 'N/A'
-      ? dummyPosterLink
-      : movie.Poster;
+  const { Title, Poster, Year, Type, imdbID } = movie;
+
+  const posterLink = Poster === 'N/A' ? dummyPosterLink : Poster;
 
   return (
-    <Link to={`/movie/${movie.imdbID}`}>
-      <article className='group flex h-full cursor-pointer flex-col gap-2 rounded-xl hover:scale-103 transition-all inset-shadow-xl inset-shadow-xl inset-shadow-indigo-500 '>
+    <Link to={`/movie/${imdbID}`}>
+      <article className='group inset-shadow-xl inset-shadow-xl flex h-full cursor-pointer flex-col gap-2 rounded-xl inset-shadow-indigo-500 transition-all hover:scale-103'>
         <div className='flex w-full justify-center rounded-xl border border-gray-300'>
           <img
             src={posterLink}
-            alt={`${movie.Title} poster`}
+            alt={`${Title} poster`}
             loading='lazy'
-            onError={(e)=> {
+            onError={(e) => {
               e.currentTarget.src = dummyPosterLink;
             }}
-            className='aspect-2/3 h-full w-full rounded-xl object-cover object-center group-hover:shadow-xl  group-hover:shadow-indigo-200/50'
+            className='aspect-2/3 h-full w-full rounded-xl object-cover object-center group-hover:shadow-xl group-hover:shadow-indigo-200/50'
           />
         </div>
         <div className='flex flex-col items-start'>
-          <h3 className='mb-2 line-clamp-1 text-lg font-bold text-ellipsis group-hover:text-indigo-600' title={movie.Title}>
-            {movie.Title}
+          <h3
+            className='mb-2 line-clamp-1 text-lg font-bold text-ellipsis group-hover:text-indigo-600'
+            title={Title}
+          >
+            {Title}
           </h3>
           <div className='flex flex-row items-center gap-2 text-sm'>
-            <p className='text-gray-600'>{movie.Year}</p>
+            <p className='text-gray-600'>{Year}</p>
             <span className='h-1 w-1 translate-y-px rounded-xl bg-gray-700'></span>
-            <p className='text-gray-600'>{movie.Type}</p>
+            <p className='text-gray-600'>{Type}</p>
           </div>
         </div>
       </article>
