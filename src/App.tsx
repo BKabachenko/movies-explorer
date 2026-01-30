b8e43754620d762e4b519f59b2b01de63fa573ed
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-01-30 11:58:27 +0200

    fix(App): remove unused page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home/Home";
-import AboutUs from "./pages/AboutUs/AboutUs";
 import MovieDetails from "./pages/MovieDetails/MovieDetails";
 import MainLayout from "./layouts/MainLayout/MainLayout";
 import SearchPage from "./pages/SearchPage/SearchPage";
@@ -10,7 +9,6 @@ function App() {
     <Routes>
       <Route element={<MainLayout />}>
         <Route index element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
         <Route path="movie/:id" element={<MovieDetails />} />
         <Route path="/search" element={<SearchPage />} />
       </Route>
