3a46c2fac3adf35e5d9408ee8257a96c45ae4989
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-01-30 11:21:32 +0200

    style(OneMovie): complete style

--- src/pages/Home/OneMovie.tsx
+++ src/pages/Home/OneMovie.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router';
 
+import Badge from '../../components/Badge/Badge';
 import type { MovieFull } from '../../types';
 
 interface MovieOneProps {
@@ -9,32 +10,37 @@ interface MovieOneProps {
 const OneMovie = ({ movie }: MovieOneProps) => {
   const dummyPosterLink = 'https://dummyimage.com/300x450/787878/ffffff&text=No+Preview';
 
-  const { Title, Poster, Year, Rated, imdbID } = movie[0];
+  const { Title, Poster, imdbRating, imdbID, Plot } = movie[0];
 
-  const posterLink = Poster === 'N/sA' ? dummyPosterLink : Poster;
+  const posterLink = Poster === 'N/A' ? dummyPosterLink : Poster;
 
   return (
-    <div className='h-full w-full rounded-xl bg-indigo-950 p-5 md:p-12'>
-      <Link to={`/movie/${imdbID}`}>
-        <article className='group inset-shadow-xl inset-shadow-xl flex h-full cursor-pointer flex-row gap-2 rounded-xl inset-shadow-indigo-500'>
-          <div className='flex w-full max-w-60 justify-center rounded-xl border border-gray-300'>
+    <div className='relative h-full w-full overflow-hidden rounded-xl bg-indigo-900 p-5 md:p-12'>
+      <Link to={`/movie/${imdbID}`} className='group'>
+        <div className='absolute inset-0 bg-linear-to-r from-gray-900 via-gray-900/80 to-transparent transition group-hover:from-gray-800'></div>
+        <article className='inset-shadow-xl inset-shadow-xl relative z-10 flex h-full cursor-pointer flex-row gap-2 rounded-xl inset-shadow-indigo-500 md:gap-x-10'>
+          <div className='flex w-full max-w-60 flex-1 justify-center rounded-xl border border-gray-300'>
             <img
               src={posterLink}
               alt={`${Title} poster`}
-              loading='lazy'
               onError={(e) => {
                 e.currentTarget.src = dummyPosterLink;
               }}
               className='aspect-2/3 h-full w-full rounded-xl object-cover object-center transition-all group-hover:shadow-xl group-hover:shadow-indigo-200/50'
             />
           </div>
-          <div className='flex flex-col items-start'>
+          <div className='flex flex-1 flex-col items-start justify-center gap-2'>
+            <div className='mb-5 flex flex-col gap-2 sm:flex-row'>
+              <Badge variant='genre'>Movie of the Day</Badge>
+              <Badge variant='rate'>{imdbRating}</Badge>
+            </div>
             <h3
-              className='mb-2 line-clamp-3 text-lg font-bold text-ellipsis text-white'
+              className='mb-2 line-clamp-3 text-xl font-bold text-ellipsis text-white sm:text-2xl md:text-4xl'
               title={Title}
             >
               {Title}
             </h3>
+            <p className='mb-2 line-clamp-3 text-sm text-ellipsis text-white'>{Plot}</p>
           </div>
         </article>
       </Link>
