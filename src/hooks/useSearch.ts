f085aa1e0ae09e6be591a11515cf849526284b88
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-02 14:42:04 +0200

    refactor(SearchPage): replace movie fetching logic with useSearch hook

---
+++ src/hooks/useSearch.ts
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+
+import type { MovieShort, SearchResponse } from '@/types';
+
+import { searchMovie } from '@/api/movies';
+
+export const useSearch = (searchText: string | undefined) => {
+  const [movies, setMovies] = useState<MovieShort[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const fetchMovies = async (title: string) => {
+      try {
+        setIsLoading(true);
+        setError(null);
+
+        const data: SearchResponse = await searchMovie(title, signal);
+        const filteredData = data.Search.filter(
+          (movie, index, self) => index === self.findIndex((m) => m.imdbID === movie.imdbID)
+        );
+        setMovies(filteredData);
+      } catch (error) {
+        if (error instanceof Error) {
+          if (error.name === 'AbortError') {
+            return;
+          }
+          setError(error.message);
+        } else {
+          setError('An unexpected error occurred');
+        }
+      } finally {
+        if (!signal.aborted) {
+          setIsLoading(false);
+        }
+      }
+    };
+
+    if (searchText && searchText.trim().length > 0) {
+      fetchMovies(searchText);
+    } else {
+      setMovies([]);
+      setError(null);
+    }
+
+    return () => {
+      controller.abort();
+    };
+  }, [searchText]);
+
+  return { movies, isLoading, error };
+};
--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,48 +1,16 @@
-import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router';
 
-import { searchMovie } from '../../api/movies';
+import { useSearch } from '@/hooks/useSearch';
+
 import BackBtn from '../../components/BackBtn/BackBtn';
 import MovieList from '../../components/MovieList/MovieList';
-import type { MovieShort, SearchResponse } from '../../types';
 
 const SearchPage = () => {
-  const [movies, setMovies] = useState<MovieShort[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-
   const [searchParams] = useSearchParams();
   const searchText = searchParams.get('search') || '';
 
-  useEffect(() => {
-    const fetchMovies = async (title: string) => {
-      try {
-        setIsLoading(true);
-        setError(null);
+  const { movies, isLoading, error } = useSearch(searchText);
 
-        const data: SearchResponse = await searchMovie(title);
-        const filteredData = data.Search.filter(
-          (movie, index, self) => index === self.findIndex((m) => m.imdbID === movie.imdbID)
-        );
-        setMovies(filteredData);
-      } catch (error) {
-        if (error instanceof Error) {
-          setError(error.message);
-        } else {
-          setError('Unknown error');
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    if (searchText) {
-      fetchMovies(searchText);
-    } else {
-      setMovies([]);
-      setError(null);
-    }
-  }, [searchText]);
   return (
     <>
       <BackBtn />
