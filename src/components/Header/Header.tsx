3f67db96fc6df655e72beb5ac394939c91743251
Bogdan ProBook <bogdankabachenko2015@gmail.com>
2026-02-04 18:44:24 +0200

    style(Header): improve layout and structure of header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -26,10 +26,10 @@ const Header = () => {
   };
 
   return (
-    <header className='sticky top-0 z-99 mb-4 flex w-full justify-around bg-white/80 backdrop-blur-md'>
-      <div className='flex max-w-7xl flex-1 flex-col gap-2 p-2 px-10 sm:py-4 md:flex-row md:py-6'>
-        <div className='flex flex-1 justify-center'>
-          <Link to='/' className='group flex flex-row'>
+    <header className='sticky top-0 z-99 mb-4 flex w-full justify-center bg-white/80 backdrop-blur-md'>
+      <div className='flex w-full max-w-7xl flex-wrap gap-2 p-2 px-10 sm:py-4 md:flex-row md:flex-nowrap md:py-6'>
+        <div className='order-1 flex flex-1 justify-center md:order-2 md:flex-1'>
+          <Link to='/' className='group flex flex-row' viewTransition>
             <div className='flex h-8 w-8 content-center justify-center rounded-lg bg-indigo-800 transition-all group-hover:scale-110'>
               <Icon src={logo} className='h-full w-6 text-white' />
             </div>
@@ -38,13 +38,15 @@ const Header = () => {
             </p>
           </Link>
         </div>
-        <div className='flex-5 pr-6'>
+
+        <div className='order-3 w-full md:order-2'>
           <SearchInput onSearch={onSearch} initialValue={searchText} />
         </div>
-        <Link to='/favorite'>
+
+        <Link to='/favorite' className='order-2 flex flex-1 justify-center md:order-3' viewTransition>
           <div className='flex w-fit cursor-pointer flex-row items-center gap-2 rounded-xl bg-indigo-50 p-1 px-3 hover:bg-indigo-200'>
             <Icon src={heartIcon} className='h-5 w-5' />
-            Favorites{' '}
+            Favorites
             {favoriteCount ? (
               <span className='rounded-lg bg-indigo-300 px-2'>{favoriteCount}</span>
             ) : (
