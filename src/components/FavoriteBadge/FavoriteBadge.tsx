e968ab8b9f572b2dfcbc767b91f005d8ded95266
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-04 15:37:34 +0200

    feat(Favoritebadge): implement add and remove logic from store

--- src/components/FavoriteBadge/FavoriteBadge.tsx
+++ src/components/FavoriteBadge/FavoriteBadge.tsx
@@ -1,28 +1,46 @@
-import type { MouseEvent } from 'react';
+import { type MouseEvent } from 'react';
 
-import heartIcon from '@/assets/icons/heart.svg?react';
+import checkIcon from '@/assets/icons/check.svg?react';
+import plusIcon from '@/assets/icons/plus.svg?react';
 import { useFavoritesStore } from '@/store/useFavoritesStore';
 
 import Icon from '../Icon/Icon';
+import type { MovieShort } from '@/types';
 
 interface FavoriteBadgeProps {
-  id: string;
+  movie: MovieShort;
 }
 
-const FavoriteBadge = ({ id }: FavoriteBadgeProps) => {
+const FavoriteBadge = ({ movie }: FavoriteBadgeProps) => {
   const addMovie = useFavoritesStore((s) => s.addMovie);
+  const removeMovie = useFavoritesStore((s) => s.removeMovie);
+  const isFavorite = useFavoritesStore((s) => s.movieList.some(e => {return e.imdbID === movie.imdbID}));
 
   const onClickHandler = (e: MouseEvent) => {
     e?.preventDefault();
-    addMovie(id);
+
+    if (isFavorite) {
+      removeMovie(movie.imdbID);
+    } else {
+      console.log(movie)
+      addMovie(movie);
+    }
   };
+
+  if (!movie) {
+    return;
+  }
 
   return (
     <button
       className='absolute top-5 right-5 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-white/50 transition group-hover:bg-white'
       onClick={onClickHandler}
     >
-      <Icon src={heartIcon} size='md' className='text-indigo-600' />
+      {isFavorite ? (
+        <Icon src={checkIcon} size='md' className='text-indigo-600' />
+      ) : (
+        <Icon src={plusIcon} size='md' className='text-indigo-600' />
+      )}
     </button>
   );
 };
