d25155171c79be5b9c32ae6db7166703f8c2155b
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-04 15:35:42 +0200

    feat(useFavoritesStore): refactor movie management functions to handle MovieShort type

--- src/store/useFavoritesStore.ts
+++ src/store/useFavoritesStore.ts
@@ -1,18 +1,35 @@
+import type { MovieShort } from '@/types';
 import { create } from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
 
 interface useFavoritesStoreState {
-  movieList: string[];
-  addMovie: (id: string) => void;
-  // isMovie: (id: string) => boolean;
+  movieList: MovieShort[];
+  addMovie: (movie: MovieShort) => void;
+  removeMovie: (id: string) => void;
+  checkIsFavorite: (id: string) => boolean;
 }
 
 export const useFavoritesStore = create<useFavoritesStoreState>()(
   devtools(
     persist(
-      (set) => ({
+      (set, get) => ({
         movieList: [],
-        addMovie: (id) => set((state) => ({ movieList: [...state.movieList, id] })),
+
+        addMovie: (movie) => {
+          const currentList = get().movieList;
+
+          if (!currentList.some(e => {return e.imdbID === movie.imdbID})) {
+            set((state) => ({ movieList: [...state.movieList, movie] }));
+          }
+        },
+
+        removeMovie: (id) =>
+          set((state) => ({ movieList: state.movieList.filter((e) => e.imdbID !== id) })),
+
+        checkIsFavorite: (id) => {
+          return (get().movieList.some(e => {return e.imdbID === id}));
+        },
+        
       }),
       {
         name: 'FavoritesStore',
