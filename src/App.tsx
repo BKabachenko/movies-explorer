68c8579f87f0cb1c415ff498f73b5f8e2ee472be
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-04 15:35:55 +0200

    feat(App): add route for Favorites page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home/Home";
 import MovieDetails from "./pages/MovieDetails/MovieDetails";
 import MainLayout from "./layouts/MainLayout/MainLayout";
 import SearchPage from "./pages/SearchPage/SearchPage";
+import Favorites from "./pages/Favorites/Favorites";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="movie/:id" element={<MovieDetails />} />
         <Route path="/search" element={<SearchPage />} />
+        <Route path="/favorite" element={<Favorites />} />
       </Route>
     </Routes>
   );
