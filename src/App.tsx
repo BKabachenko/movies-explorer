dbfd594f5274e2898187a5ca76aa28ebed8b04cf
Bogdan ProBook <bogdankabachenko2015@gmail.com>
2026-02-04 19:14:22 +0200

    feat(App): implement lazy loading and add Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import { Route, Routes } from "react-router";
-import Home from "./pages/Home/Home";
-import MovieDetails from "./pages/MovieDetails/MovieDetails";
-import MainLayout from "./layouts/MainLayout/MainLayout";
-import SearchPage from "./pages/SearchPage/SearchPage";
-import Favorites from "./pages/Favorites/Favorites";
+import { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router';
+
+import MainLayout from './layouts/MainLayout/MainLayout';
+
+const Home = lazy(() => import('@/pages/Home/Home'));
+const MovieDetails = lazy(() => import('@/pages/MovieDetails/MovieDetails'));
+const SearchPage = lazy(() => import('@/pages/SearchPage/SearchPage'));
+const Favorites = lazy(() => import('@/pages/Favorites/Favorites'));
 
 function App() {
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="movie/:id" element={<MovieDetails />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/favorite" element={<Favorites />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div className=''>Loader...</div>}>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path='movie/:id' element={<MovieDetails />} />
+          <Route path='/search' element={<SearchPage />} />
+          <Route path='/favorite' element={<Favorites />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
