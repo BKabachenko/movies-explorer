5352e9d7be9c1bfbb2cf65ebd81434e93cbad7bb
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-04 15:36:30 +0200

    feat(Favorites): create favorites page

---
+++ src/pages/Favorites/Favorites.tsx
@@ -0,0 +1,40 @@
+import heartIcon from '@/assets/icons/heart.svg?react';
+import BackBtn from '@/components/BackBtn/BackBtn';
+import Icon from '@/components/Icon/Icon';
+import MovieList from '@/components/MovieList/MovieList';
+import { useFavoritesStore } from '@/store/useFavoritesStore';
+
+const emptyList = () => {
+  return (
+    <>
+      <div className='flex flex-col pt-15 text-center'>
+        <h3 className='text-2xl font-semibold text-gray-900'>Oh no, list is empty!</h3>
+        <p className='font-semibold text-gray-500'>
+          Add movies to favorite and then you can see they in this list.
+        </p>
+      </div>
+    </>
+  );
+};
+
+const Favorites = () => {
+  const movieList = useFavoritesStore((s) => s.movieList);
+
+  return (
+    <div className='animate-fade-in'>
+      <BackBtn />
+      <div className='mb-5 flex flex-row items-center gap-5'>
+        <div className='flex items-center justify-center rounded-xl bg-indigo-300/50 p-2'>
+          <Icon src={heartIcon} size='lg' className='fill-indigo-600 text-indigo-600' />
+        </div>
+        <div className='flex flex-col'>
+          <h3 className='text-2xl font-semibold text-gray-900'>Your Favorites</h3>
+          <p className='font-semibold text-gray-500'>Movies you have saved for later</p>
+        </div>
+      </div>
+      {!movieList || !movieList.length ? emptyList() : <MovieList list={movieList} />}
+    </div>
+  );
+};
+
+export default Favorites;
