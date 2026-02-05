0f1b3c23a5132502fb233059782ccf1d599b9ab1
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-05 17:02:03 +0200

    refactor(FavoriteBadge): improve code readability and formatting

--- src/components/FavoriteBadge/FavoriteBadge.tsx
+++ src/components/FavoriteBadge/FavoriteBadge.tsx
@@ -1,11 +1,12 @@
 import { type MouseEvent } from 'react';
 
+import type { MovieShort } from '@/types';
+
 import checkIcon from '@/assets/icons/check.svg?react';
 import plusIcon from '@/assets/icons/plus.svg?react';
 import { useFavoritesStore } from '@/store/useFavoritesStore';
 
 import Icon from '../Icon/Icon';
-import type { MovieShort } from '@/types';
 
 interface FavoriteBadgeProps {
   movie: MovieShort;
@@ -14,7 +15,12 @@ interface FavoriteBadgeProps {
 const FavoriteBadge = ({ movie }: FavoriteBadgeProps) => {
   const addMovie = useFavoritesStore((s) => s.addMovie);
   const removeMovie = useFavoritesStore((s) => s.removeMovie);
-  const isFavorite = useFavoritesStore((s) => s.movieList.some(e => {return e.imdbID === movie.imdbID}));
+
+  const isFavorite = useFavoritesStore((s) =>
+    s.movieList.some((e) => {
+      return e.imdbID === movie.imdbID;
+    })
+  );
 
   const onClickHandler = (e: MouseEvent) => {
     e?.preventDefault();
@@ -22,7 +28,7 @@ const FavoriteBadge = ({ movie }: FavoriteBadgeProps) => {
     if (isFavorite) {
       removeMovie(movie.imdbID);
     } else {
-      console.log(movie)
+      console.log(movie);
       addMovie(movie);
     }
   };
@@ -35,6 +41,7 @@ const FavoriteBadge = ({ movie }: FavoriteBadgeProps) => {
     <button
       className='absolute top-5 right-5 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-white/50 transition group-hover:bg-white'
       onClick={onClickHandler}
+      aria-label={isFavorite ? 'Remove from favorites' : 'Add to favorites'}
     >
       {isFavorite ? (
         <Icon src={checkIcon} size='md' className='text-indigo-600' />
