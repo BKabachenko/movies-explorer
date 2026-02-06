eaeeb6ebdc1184778f9d752acee9392f654e3f3f
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-06 15:13:46 +0200

    test(FavoriteBadge): add tests to check init and toggle component state

--- src/components/FavoriteBadge/FavoriteBadge.test.tsx
+++ src/components/FavoriteBadge/FavoriteBadge.test.tsx
@@ -1,10 +1,14 @@
-import { useFavoritesStore } from '@/store/useFavoritesStore';
-import { render, screen, fireEvent } from '@testing-library/react';
-import { beforeEach, describe, it } from 'vitest';
-import FavoriteBadge from './FavoriteBadge';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { beforeEach, describe, expect, it } from 'vitest';
+
 import type { MovieShort } from '@/types';
 
-const movieMock:MovieShort = {
+import { useFavoritesStore } from '@/store/useFavoritesStore';
+
+import FavoriteBadge from './FavoriteBadge';
+
+const movieMock: MovieShort = {
   imdbID: 'tt123',
   Title: 'Matrix',
   Year: '1999',
@@ -13,23 +17,36 @@ const movieMock:MovieShort = {
 };
 
 describe('FavoriteBadge Component', () => {
-  
-  beforeEach(()=> {
+  beforeEach(() => {
     localStorage.clear();
-    useFavoritesStore.setState({movieList: []});
-  })
-  
-  it('should render "Add" btn init', ()=> {
+    useFavoritesStore.setState({ movieList: [] });
+  });
+
+  it('should render "Add" btn init', () => {
     render(<FavoriteBadge movie={movieMock} />);
-    
+    const btn = screen.getByLabelText('Add to favorites');
+    expect(btn).toBeInTheDocument();
   });
-  it('', ()=> {
 
-  });
-  it('', ()=> {
+  it('should toggle state on click', async () => {
+    const user = userEvent.setup();
 
-  });
-  it('', ()=> {
+    render(<FavoriteBadge movie={movieMock} />);
+    let btn = screen.getByLabelText('Add to favorites');
+    expect(btn).toBeInTheDocument();
 
+    await user.click(btn);
+
+    btn = screen.getByLabelText('Remove from favorites');
+    expect(btn).toBeInTheDocument();
+
+    expect(useFavoritesStore.getState().movieList[0]).toEqual(movieMock);
+
+    await user.click(btn);
+
+    btn = screen.getByLabelText('Add to favorites');
+    expect(btn).toBeInTheDocument();
+
+    expect(useFavoritesStore.getState().movieList).toHaveLength(0);
   });
 });
