2eb20a3a4025da2de1c6e285719006e54977f802
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-09 17:17:03 +0200

    chore: update dependencies and configure babel-plugin-react-compiler in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 /// <reference types="vitest/config" />
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+    tailwindcss(),
+    svgr(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
