fdb7240a9b8c958cc9f8eeae34c1e92e6193034b
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-11 11:43:49 +0200

    feat(SkeletonCard): add SkeletonCard component for loading placeholders

---
+++ src/components/MovieCard/MovieCardSkeleton.tsx
@@ -0,0 +1,15 @@
+import Skeleton from '../Skeleton/Skeleton';
+
+const MovieCardSkeleton = () => {
+  return (
+    <div className='flex h-100 w-full flex-col gap-2'>
+      <Skeleton className='aspect-2/3 w-full' />
+      <div className='flex flex-col items-start gap-2'>
+        <Skeleton className='h-7 w-full' />
+        <Skeleton className='h-4 w-1/2' />
+      </div>
+    </div>
+  );
+};
+
+export default MovieCardSkeleton;
