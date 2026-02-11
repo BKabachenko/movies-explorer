3fb5c80da2321450aa5af82d37f843ddf0950af8
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-11 11:44:37 +0200

    feat(SearchPage): enhance loading state with MovieListSkeleton components

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,5 +1,6 @@
 import { useSearchParams } from 'react-router';
 
+import MovieListSkeleton from '@/components/MovieList/MovieListSkeleton';
 import { useSearch } from '@/hooks/useSearch';
 
 import BackBtn from '../../components/BackBtn/BackBtn';
@@ -14,8 +15,7 @@ const SearchPage = () => {
   return (
     <>
       <BackBtn />
-      {isLoading && <h3>Please wait... Your list are loading.</h3>}
-      {searchText === null && <h3>Please type your request title.</h3>}
+      {searchText === null && <MovieListSkeleton count={5} />}
       {error && searchText && <h3>Error. {error}</h3>}
       {!isLoading && !error && <MovieList list={movies} />}
     </>
