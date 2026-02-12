464d42c9eac8b6fa1da0ba0d57a5bb911e6faccc
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-12 15:34:13 +0200

    feat(Button): add styles to variants

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -10,8 +10,8 @@ const defaultStyles =
   'flex cursor-pointer flex-row items-center justify-center gap-2 rounded-lg px-4 py-2 font-medium font-semibold transition-all duration-200';
 
 const variantStyles = {
-  addToFavorite: '',
-  removeFromFavorite: '',
+  addToFavorite: 'py-2 px-4 border border-gray-300 text-white bg-indigo-600/50 hover:bg-indigo-500  shadow-lg active:shadow-indigo-400',
+  removeFromFavorite: 'py-2 px-4 border border-gray-300 text-white bg-indigo-600/50 hover:bg-indigo-500 shadow-lg active:shadow-indigo-400',
   backBtn:
     'border border-gray-200 bg-white p-2 text-gray-500 shadow-sm hover:bg-indigo-50 hover:text-indigo-600 active:bg-indigo-200 active:text-indigo-600',
 };
