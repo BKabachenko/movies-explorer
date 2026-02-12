57826b68b424831d1b8d4a375b578b0fb4279e07
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-12 12:41:29 +0200

    refactor(FavoriteBadge): move store logic to new hook

--- src/components/FavoriteBadge/FavoriteBadge.tsx
+++ src/components/FavoriteBadge/FavoriteBadge.tsx
@@ -1,10 +1,8 @@
-import { type MouseEvent } from 'react';
-
 import type { MovieShort } from '@/types';
 
 import checkIcon from '@/assets/icons/check.svg?react';
 import plusIcon from '@/assets/icons/plus.svg?react';
-import { useFavoritesStore } from '@/store/useFavoritesStore';
+import { useFavoriteMovie } from '@/hooks/useFavoriteMovie';
 
 import Icon from '../Icon/Icon';
 
@@ -14,33 +12,16 @@ interface FavoriteBadgeProps {
 }
 
 const FavoriteBadge = ({ movie, className }: FavoriteBadgeProps) => {
-  const addMovie = useFavoritesStore((s) => s.addMovie);
-  const removeMovie = useFavoritesStore((s) => s.removeMovie);
-
-  const isFavorite = useFavoritesStore((s) =>
-    s.movieList.some((e) => {
-      return e.imdbID === movie.imdbID;
-    })
-  );
-
-  const onClickHandler = (e: MouseEvent) => {
-    e?.preventDefault();
-
-    if (isFavorite) {
-      removeMovie(movie.imdbID);
-    } else {
-      addMovie(movie);
-    }
-  };
+  const { isFavorite, toggleFavorite } = useFavoriteMovie(movie);
 
   if (!movie) {
-    return;
+    return null;
   }
 
   return (
     <button
       className={`${className} absolute top-5 right-5 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-white/50 transition group-hover:bg-white`}
-      onClick={onClickHandler}
+      onClick={toggleFavorite}
       aria-label={isFavorite ? 'Remove from favorites' : 'Add to favorites'}
     >
       {isFavorite ? (
