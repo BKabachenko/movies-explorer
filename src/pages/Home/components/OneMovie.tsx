6ecdc8b24ace85ccde81f1ec0070e1bb261bf2a5
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-12 12:06:23 +0200

    feat(OneMovie): add onLoad and onError handler

--- src/pages/Home/components/OneMovie.tsx
+++ src/pages/Home/components/OneMovie.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import { Link } from 'react-router';
 
 import type { MovieFull } from '@/types';
 
 import Badge from '@/components/Badge/Badge';
+import Skeleton from '@/components/Skeleton/Skeleton';
 import { DEFAULT_POSTER } from '@/constants';
 
 interface MovieOneProps {
@@ -12,21 +14,31 @@ interface MovieOneProps {
 const OneMovie = ({ movie }: MovieOneProps) => {
   const { Title, Poster, imdbRating, imdbID, Plot } = movie;
 
-  const posterLink = Poster === 'N/A' ? DEFAULT_POSTER : Poster;
+  const [imgSrc, setImgSrc] = useState<string>(Poster === 'N/A' ? DEFAULT_POSTER : Poster);
+  const [isImgLoad, setIsImgLoad] = useState<boolean>(false);
+
+  const imgLoaderStyle = isImgLoad ? 'opacity-100' : 'opacity-0';
+
+  const imgErrorHandler = () => {
+    setImgSrc(DEFAULT_POSTER);
+  };
+  const imgLoadHandler = () => {
+    setIsImgLoad(true);
+  };
 
   return (
     <div className='relative h-full w-full overflow-hidden rounded-xl bg-indigo-900 p-5 md:p-12'>
       <Link to={`/movie/${imdbID}`} className='group'>
         <div className='absolute inset-0 bg-linear-to-r from-gray-900 via-gray-900/80 to-transparent transition group-hover:from-gray-800'></div>
         <article className='inset-shadow-xl inset-shadow-xl relative z-10 flex h-full cursor-pointer flex-row gap-2 rounded-xl inset-shadow-indigo-500 md:gap-x-10'>
-          <div className='flex w-full max-w-60 flex-1 justify-center rounded-xl border border-gray-300'>
+          <div className='relative flex max-w-60 flex-1 justify-center rounded-xl border border-gray-300'>
+            {!isImgLoad && <Skeleton className='absolute inset-0 aspect-2/3 h-full w-full' />}
             <img
-              src={posterLink}
+              src={imgSrc}
               alt={`${Title} poster`}
-              onError={(e) => {
-                e.currentTarget.src = DEFAULT_POSTER;
-              }}
-              className='aspect-2/3 h-full w-full rounded-xl object-cover object-center transition-all group-hover:shadow-xl group-hover:shadow-indigo-200/50'
+              onLoad={imgLoadHandler}
+              onError={imgErrorHandler}
+              className={`${imgLoaderStyle} aspect-2/3 h-full w-full rounded-xl object-cover object-center transition-all group-hover:shadow-xl group-hover:shadow-indigo-200/50`}
             />
           </div>
           <div className='flex flex-1 flex-col items-start justify-center gap-2'>
