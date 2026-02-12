e29a7782665bf33a9ffe650a4dac7e81f43f4250
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-12 11:42:45 +0200

    feat(MovieCard): add onLoad handler

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router';
 
 import type { MovieShort } from '@/types';
@@ -5,6 +6,7 @@ import type { MovieShort } from '@/types';
 import { DEFAULT_POSTER } from '@/constants';
 
 import FavoriteBadge from '../FavoriteBadge/FavoriteBadge';
+import Skeleton from '../Skeleton/Skeleton';
 
 type MovieCardProps = {
   movie: MovieShort;
@@ -13,21 +15,31 @@ type MovieCardProps = {
 const MovieCard = ({ movie }: MovieCardProps) => {
   const { Title, Poster, Year, Type, imdbID } = movie;
 
-  const posterLink = Poster === 'N/A' ? DEFAULT_POSTER : Poster;
+  const [imgSrc, setImgSrc] = useState<string>(Poster === 'N/A' ? DEFAULT_POSTER : Poster);
+  const [isImgLoad, setIsImgLoad] = useState<boolean>(false);
+
+  const imgErrorHandler = () => {
+    setImgSrc(DEFAULT_POSTER);
+  };
+  const imgLoadHandler = () => {
+    setIsImgLoad(true);
+  };
+
+  const imgLoaderStyle = isImgLoad ? 'opacity-100' : 'opacity-0';
 
   return (
     <Link to={`/movie/${imdbID}`}>
-      <article className='group inset-shadow-xl inset-shadow-xl relative flex h-full cursor-pointer flex-col gap-2 rounded-xl inset-shadow-indigo-500 transition-all hover:scale-103'>
-        <FavoriteBadge movie={movie} />
-        <div className='flex w-full justify-center rounded-xl border border-gray-300'>
+      <article className='group inset-shadow-xl relative flex h-full cursor-pointer flex-col gap-2 rounded-xl inset-shadow-indigo-500 transition-all hover:scale-103'>
+        <FavoriteBadge movie={movie} className='z-10' />
+        <div className='relative flex w-full justify-center rounded-xl border border-gray-300'>
+          {!isImgLoad && <Skeleton className='absolute inset-0 aspect-2/3 h-full w-full' />}
           <img
-            src={posterLink}
+            src={imgSrc}
             alt={`${Title} poster`}
             loading='lazy'
-            onError={(e) => {
-              e.currentTarget.src = DEFAULT_POSTER;
-            }}
-            className='aspect-2/3 h-full w-full rounded-xl object-cover object-center group-hover:shadow-xl group-hover:shadow-indigo-200/50'
+            onLoad={imgLoadHandler}
+            onError={imgErrorHandler}
+            className={`${imgLoaderStyle} aspect-2/3 h-full w-full rounded-xl object-cover object-center transition-opacity duration-500 group-hover:shadow-xl group-hover:shadow-indigo-200/50`}
           />
         </div>
         <div className='flex flex-col items-start'>
