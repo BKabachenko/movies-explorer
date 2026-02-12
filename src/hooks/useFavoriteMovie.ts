d8e4c972f9074a976d46ba2f966765fff2c42151
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-12 12:41:01 +0200

    feat(useFavoriteMovie): implement new hook to toggle movie in favoriteStore

---
+++ src/hooks/useFavoriteMovie.ts
@@ -0,0 +1,27 @@
+import type { MovieShort } from '@/types';
+
+import { useFavoritesStore } from '@/store/useFavoritesStore';
+
+export const useFavoriteMovie = ( movie: MovieShort ) => {
+  const addMovie = useFavoritesStore((s) => s.addMovie);
+  const removeMovie = useFavoritesStore((s) => s.removeMovie);
+
+  const isFavorite = useFavoritesStore((s) =>
+    s.movieList.some((e) => {
+      return e.imdbID === movie.imdbID;
+    })
+  );
+
+  const toggleFavorite = (e: React.MouseEvent) => {
+    e?.preventDefault();
+    e?.stopPropagation();
+
+    if (isFavorite) {
+      removeMovie(movie.imdbID);
+    } else {
+      addMovie(movie);
+    }
+  };
+
+  return { isFavorite, toggleFavorite };
+};
