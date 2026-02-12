5ab6ece8c3855b4c62d92f561c79bac60ef4963a
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-12 17:55:31 +0200

    feat(Pagination): implement page param on search and add Pagination component

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -5,19 +5,33 @@ import { useSearch } from '@/hooks/useSearch';
 
 import BackBtn from '../../components/BackBtn/BackBtn';
 import MovieList from '../../components/MovieList/MovieList';
+import Pagination from '@/components/Pagination/Pagination';
 
 const SearchPage = () => {
   const [searchParams] = useSearchParams();
   const searchText = searchParams.get('search') || '';
+  const searchPageParam = searchParams.get('page') || '1';
+  const searchPageNumber = parseInt(searchPageParam); 
 
-  const { movies, isLoading, error } = useSearch(searchText);
+  const { movies, isLoading, error } = useSearch(searchText, searchPageNumber);
+
+  if(isLoading) {
+    return null;
+  }
 
   return (
     <>
       <BackBtn />
       {searchText === null && <MovieListSkeleton count={5} />}
       {error && searchText && <h3>Error. {error}</h3>}
-      {!isLoading && !error && <MovieList list={movies} />}
+      {!isLoading && !error && (
+        <>
+          <MovieList list={movies} />
+          <div className='flex justify-center pt-10'>
+            <Pagination maxValue={100}/>
+          </div>
+        </>
+      )}
     </>
   );
 };
