0ef2afc0c76d1b9e2f57ab887fb913c79a36e0e6
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-16 17:48:16 +0200

    refactor(App): enhance loading experience with skeletons  component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Route, Routes } from 'react-router';
 
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import MainLayout from './layouts/MainLayout/MainLayout';
+import HomeSkeleton from './pages/Home/HomeSkeleton';
+import MovieDetailsSkeleton from './pages/MovieDetails/MovieDetailsSkeleton';
+import SearchPageSkeleton from './pages/SearchPage/SearchPageSkeleton';
 
 const Home = lazy(() => import('@/pages/Home/Home'));
 const MovieDetails = lazy(() => import('@/pages/MovieDetails/MovieDetails'));
@@ -12,16 +15,35 @@ const Favorites = lazy(() => import('@/pages/Favorites/Favorites'));
 function App() {
   return (
     <ErrorBoundary>
-      <Suspense fallback={<div className=''>Loader...</div>}>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route index element={<Home />} />
-            <Route path='movie/:id' element={<MovieDetails />} />
-            <Route path='/search' element={<SearchPage />} />
-            <Route path='/favorite' element={<Favorites />} />
-          </Route>
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route
+            index
+            element={
+              <Suspense fallback={<HomeSkeleton />}>
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route
+            path='movie/:id'
+            element={
+              <Suspense fallback={<MovieDetailsSkeleton />}>
+                <MovieDetails />
+              </Suspense>
+            }
+          />
+          <Route
+            path='/search'
+            element={
+              <Suspense fallback={<SearchPageSkeleton />}>
+                <SearchPage />
+              </Suspense>
+            }
+          />
+          <Route path='/favorite' element={<Favorites />} />
+        </Route>
+      </Routes>
     </ErrorBoundary>
   );
 }
