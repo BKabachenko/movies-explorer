ce4e6ec77acb862ed89b43fb7b19cce5b3932bb2
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-16 13:50:10 +0200

    feat: add SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 import App from './App.tsx';
 
 import './index.css';
@@ -11,5 +13,6 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <SpeedInsights />
   </StrictMode>
 );
