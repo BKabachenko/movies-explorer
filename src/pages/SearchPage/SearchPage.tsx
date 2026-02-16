ce1d3eaed3d86e8de5b23baf8480c6c37df110ab
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-16 12:04:51 +0200

    fix(SearchPage): fix skeleton show when isLoading state true

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,34 +1,35 @@
 import { useSearchParams } from 'react-router';
 
 import MovieListSkeleton from '@/components/MovieList/MovieListSkeleton';
+import Pagination from '@/components/Pagination/Pagination';
 import { useSearch } from '@/hooks/useSearch';
 
 import BackBtn from '../../components/BackBtn/BackBtn';
 import MovieList from '../../components/MovieList/MovieList';
-import Pagination from '@/components/Pagination/Pagination';
 
 const SearchPage = () => {
   const [searchParams] = useSearchParams();
   const searchText = searchParams.get('search') || '';
   const searchPageParam = searchParams.get('page') || '1';
-  const searchPageNumber = parseInt(searchPageParam); 
+  const searchPageNumber = parseInt(searchPageParam);
 
   const { movies, isLoading, error } = useSearch(searchText, searchPageNumber);
-
-  if(isLoading) {
-    return null;
-  }
 
   return (
     <>
       <BackBtn />
-      {searchText === null && <MovieListSkeleton count={5} />}
-      {error && searchText && <h3>Error. {error}</h3>}
-      {!isLoading && !error && (
+      {isLoading && <MovieListSkeleton count={10} />}
+      {error && <p className='text-center'>Error. {error} </p>}
+      {(movies.length === 0 || searchText.length === 0) && (
+        <p className='text-center'>
+          We can&#39;t search for nothing! Please type what you are looking for in the search bar.
+        </p>
+      )}
+      {!isLoading && !error && movies.length > 0 && (
         <>
           <MovieList list={movies} />
           <div className='flex justify-center pt-10'>
-            <Pagination maxValue={100}/>
+            <Pagination maxValue={100} />
           </div>
         </>
       )}
