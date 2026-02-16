01b6853d06373981f756920c7d4917ef4c28ac21
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-16 13:05:24 +0200

    style(Button): implement disabled state

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,21 +7,20 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const defaultStyles =
-  'flex cursor-pointer flex-row items-center justify-center gap-2 rounded-lg px-4 py-2 font-medium font-semibold transition-all duration-200';
+  'flex cursor-pointer flex-row items-center justify-center gap-2 rounded-lg px-4 py-2 font-medium font-semibold transition-all duration-200 disabled:cursor-not-allowed disabled:bg-gray-50/50 disabled:text-gray-400/50';
 
 const variantStyles = {
-  addToFavorite: 'py-2 px-4 border border-gray-300 text-white bg-indigo-600/50 hover:bg-indigo-500  shadow-lg active:shadow-indigo-400',
-  removeFromFavorite: 'py-2 px-4 border border-gray-300 text-white bg-indigo-600/50 hover:bg-indigo-500 shadow-lg active:shadow-indigo-400',
+  addToFavorite:
+    'py-2 px-4 border border-gray-300 text-white bg-indigo-600/50 hover:bg-indigo-500  shadow-lg active:shadow-indigo-400',
+  removeFromFavorite:
+    'py-2 px-4 border border-gray-300 text-white bg-indigo-600/50 hover:bg-indigo-500 shadow-lg active:shadow-indigo-400',
   backBtn:
     'border border-gray-200 bg-white p-2 text-gray-500 shadow-sm hover:bg-indigo-50 hover:text-indigo-600 active:bg-indigo-200 active:text-indigo-600',
 };
 
 const Button = ({ variant, children, className, ...props }: ButtonProps) => {
   return (
-    <button
-      className={`${defaultStyles} ${variantStyles[variant]} ${className}`}
-      {...props}
-    >
+    <button className={`${defaultStyles} ${variantStyles[variant]} ${className}`} {...props}>
       {children}
     </button>
   );
