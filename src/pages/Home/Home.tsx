5e98d370ef326939a67d4e23da94c18d9e7918ea
Bogdan HomePC <bogdankabachenko2015@gmail.com>
2026-02-18 16:00:17 +0200

    refactor(Home): use constants for text

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import heart from '@/assets/icons/heart.svg?react';
 import starHome from '@/assets/icons/star_home.svg?react';
 import { useMoviesFromFile } from '@/hooks/useMoviesFromFile';
+import { TEXTS } from '@/constants/strings';
 
 import HomeSkeleton from './HomeSkeleton';
 import MovieLine from './components/MovieLine';
@@ -16,7 +17,7 @@ const Home = () => {
   if (error) {
     return (
       <p className='pt-10 text-center text-xl'>
-        Oops! Something went wrong. Please try refreshing the page or try again later.
+        {TEXTS.HOME.ERROR_OOPS}
         <p>{error}</p>
       </p>
     );
@@ -29,8 +30,8 @@ const Home = () => {
           <div className='h-100 w-full'>
             <OneMovie movie={movieOne[0]} />
           </div>
-          <MovieLine label='TOP 10' movies={moviesTop} icon={starHome} />
-          <MovieLine label='Recommended for you' movies={moviesRandom} icon={heart} />
+          <MovieLine label={TEXTS.HOME.TOP_10} movies={moviesTop} icon={starHome} />
+          <MovieLine label={TEXTS.HOME.RECOMMENDED} movies={moviesRandom} icon={heart} />
         </div>
       )}
     </>
